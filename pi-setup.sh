039929ad22a25afcca82c71e1c8f861f7f86b73a
Damian Mee <spam@meedamian.com>
2018-09-22 21:00:19 +0700

    uncomment building Bitcoin

--- pi-setup.sh
+++ pi-setup.sh
@@ -32,32 +32,32 @@ sudo apt-get -y upgrade
 sudo apt-get install -y   git jq tmux miniupnpc nmap ufw tree bc
 
 
-# ### Bitcoin (from sources)
-#
-# # install all dependencies needed to build Bitcoind
-# sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev
-#
-# cd ~
-#
-# # get/update bitcoind source code
-# if [ ! -d ~/bitcoin ]; then
-#   git clone https://github.com/bitcoin/bitcoin.git
-#   cd bitcoin
-# else
-#   cd bitcoin
-#   git pull origin master
-#   git fetch --tags
-# fi
-#
-# git checkout "$(cat /home/pi/bundle/bitcoind_version)"
-#
-# ./autogen.sh
-# ./configure CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768" --enable-cxx  --disable-shared --with-pic --disable-tests --disable-bench --enable-upnp-default --without-gui --disable-wallet
-# make
-# sudo make install
-#
-# sudo /bin/cp /home/pi/bundle/bitcoind.service /etc/systemd/system/
-# sudo systemctl enable bitcoind
+### Bitcoin (from sources)
+
+# install all dependencies needed to build Bitcoind
+sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev libminiupnpc-dev libzmq3-dev
+
+cd ~
+
+# get/update bitcoind source code
+if [ ! -d ~/bitcoin ]; then
+  git clone https://github.com/bitcoin/bitcoin.git
+  cd bitcoin
+else
+  cd bitcoin
+  git pull origin master
+  git fetch --tags
+fi
+
+git checkout "$(cat /home/pi/bundle/bitcoind_version)"
+
+./autogen.sh
+./configure CXXFLAGS="--param ggc-min-expand=1 --param ggc-min-heapsize=32768" --enable-cxx  --disable-shared --with-pic --disable-tests --disable-bench --enable-upnp-default --without-gui --disable-wallet
+make
+sudo make install
+
+sudo /bin/cp /home/pi/bundle/bitcoind.service /etc/systemd/system/
+sudo systemctl enable bitcoind
 
 
 ### UFW
