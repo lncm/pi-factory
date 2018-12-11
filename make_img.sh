69a7796c530c28cb225cc314c3d294071b41406c
AnotherDroog <another_droog@protonmail.com>
2018-12-11 23:51:44 +0700

    Separate lncm-box bundle into components

--- make_img.sh
+++ make_img.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+## make_img.sh ##
+
+# Creates a zipped & partitioned image file for burning onto SD cards
+# 256MB bootable FAT32L partition with official Alpine linux and lncm-box
+# Make sure "parted", "dosfstools" and "zip" are installed
+
 # Copyright © 2018 LNCM contributors
 
 # This script, "make_img.sh" is under MIT License
@@ -21,48 +27,82 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
+VER=v0.3.0
+ALP=alpine-rpi-3.8.1-armhf.tar.gz
+IMG=lncm-box-${VER}.img
+IOT=iotwifi.tar.gz
+FIX=modloop-rpi2.tar.gz
+CACHE=cache.tar.gz
+MNT=/mnt/lncm
+
 if [ "$(id -u)" -ne "0" ]; then
-    echo "This needs to be run as root"
+    echo "This script must be run as root"
     exit 1
 fi
 
+# Todo: check for zip & unzip, mkfs.vfat, etc too
 if command -v parted 2>&1 1>/dev/null; [ "$?" -ne "0" ]; then
-  echo "'parted' package needs to be installed. If you're on Debian-based system, you can install it with:"
+  echo "'parted' package needs to be installed. If you're on a Debian-based system, you can install it with:"
   echo "	sudo apt install -y parted"
   exit 1
 fi
 
-# Creates a zipped & partitioned image file for burning onto SD cards
-# 256MB bootable FAT32L partition with official Alpine linux and lncm box
-# Make sure "parted", "dosfstools" and "zip" are installed
+echo "Building ${IMG}"
+echo "Using ${ALP} as base distribution"
+
+echo 'Generate fresh box.apkovl.tar.gz from source'
+sh make_apkovl.sh
 
 if ! [ -d lncm-workdir ]; then
   mkdir lncm-workdir
 fi
 cd lncm-workdir
-ALP=alpine-rpi-3.8.1-armhf.tar.gz
-BOX=lncm-box-v0.2.1.tar.gz
-IMG=lncm-box-v0.2.1.img
-MNT=/mnt/lncm
-# Check if already downloaded. Don't download again
+
 if ! [ -f $ALP ]; then
+  echo "${ALP} not found, fetching..."
   wget http://dl-cdn.alpinelinux.org/alpine/v3.8/releases/armhf/${ALP}
 fi
-if ! [ -f $BOX ]; then
-  wget https://github.com/lncm/pi-factory/releases/download/v0.2.1/${BOX}
+if ! [ -f $IOT ]; then
+  echo "${IOT} not found, fetching..."
+  wget https://github.com/lncm/pi-factory/releases/download/${VER}/${IOT}
 fi
-dd if=/dev/zero of=$IMG bs=1M count=256
-DEV=$(losetup -f --show $IMG)
-parted -s $DEV mklabel msdos mkpart p fat32 2048s 100% set 1 boot on
-parted -s $DEV print
+if ! [ -f $FIX ]; then
+  echo "${FIX} not found, fetching..."
+  wget https://github.com/lncm/pi-factory/releases/download/${VER}/${FIX}
+fi
+if ! [ -f $CACHE ]; then
+  echo "${CACHE} not found, fetching..."
+  wget https://github.com/lncm/pi-factory/releases/download/${VER}/${CACHE}
+fi
+
+echo "Create and mount 256MB image"
+dd if=/dev/zero of=$IMG bs=1M count=256 && \
+DEV=$(losetup -f) && \
+losetup -f $IMG && \
+echo "Create 256MB FAT32 partition and filesystem" && \
+parted -s $DEV mklabel msdos mkpart p fat32 2048s 100% set 1 boot on && \
 mkfs.vfat ${DEV}p1 -IF 32
 if ! [ -d $MNT ]; then
   mkdir $MNT
 fi
+echo "Mount FAT partition"
 mount ${DEV}p1 $MNT
+echo "Extract alpine distribution"
 tar -xzf $ALP -C ${MNT}/ --no-same-owner
-tar -xzf $BOX -C ${MNT}/ --no-same-owner
+echo "Extract iotwifi container"
+tar -xzf $IOT -C ${MNT}/ --no-same-owner
+echo "Extract cache dir for docker and avahi"
+tar -xzf $CACHE -C ${MNT}/ --no-same-owner
+echo "Patch RPi3 WiFi"
+tar -xzf $FIX -C ${MNT}/boot/ --no-same-owner
+echo "Copy latest box.apkovl tarball"
+cp ../box.apkovl.tar.gz ${MNT}
+echo "Flush writes to disk"
 sync
+echo "Unmount"
 umount $MNT
 losetup -d ${DEV}
+echo "Compress img as zip"
 zip -r ${IMG}.zip $IMG
+echo -e "Done!\nYou may flash your ${IMG}.zip using Etcher or dd the ${IMG}"
