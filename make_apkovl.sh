e32af8e1cd7db53507ca26674069badee5a898bb
Another Droog <36770425+AnotherDroog@users.noreply.github.com>
2018-12-11 18:43:17 +0700

    Remove superfluous -p option when creating archive

--- make_apkovl.sh
+++ make_apkovl.sh
@@ -3,4 +3,4 @@
 # Disables adding resource-forks on MacOS
 export COPYFILE_DISABLE=true
 
-tar cvzpf box.apkovl.tar.gz --exclude ‘.DS_Store’ etc home
+tar cvzf box.apkovl.tar.gz --exclude ‘.DS_Store’ etc home
