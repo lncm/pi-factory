eddb419419a7b8eae48bfea271754b15f7f70e06
AnotherDroog <another_droog@protonmail.com>
2018-12-28 22:03:58 +0700

    Fix README.md adding to apkovl

--- make_apkovl.sh
+++ make_apkovl.sh
@@ -3,6 +3,6 @@
 # Disables adding resource-forks on MacOS
 export COPYFILE_DISABLE=true
 
-# tar -h dereferences symlinks, i.e. add target to archive
-# on MacOS/BSD it must be invoked as tar -H
-tar czhf box.apkovl.tar.gz --exclude '.DS_Store' etc home
+cp README.md home/lncm/README.md
+
+tar czf box.apkovl.tar.gz --exclude '.DS_Store' etc home
