8bd47e24f2734a42614e3a5eaf1c4f0074200910
Damian Mee <spam@meedamian.com>
2019-02-04 16:39:27 +0700

    mv env var to the command it affects

--- make_apkovl.sh
+++ make_apkovl.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# Disables adding resource-forks on MacOS
-export COPYFILE_DISABLE=true
-
 cp README.md home/lncm/README.txt
 
-tar czf box.apkovl.tar.gz --exclude '.DS_Store' --exclude 'README.md' etc home usr
+# `COPYFILE_DISABLE=true` disables adding resource-forks on MacOS
+COPYFILE_DISABLE=true tar czf box.apkovl.tar.gz --exclude '.DS_Store' --exclude 'README.md' etc home usr
