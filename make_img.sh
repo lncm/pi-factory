9ca4d904b8b606596c276fc277c071e034836366
Damian Mee <spam@meedamian.com>
2019-02-04 14:09:11 +0700

    remove unnecessary license repetition

--- make_img.sh
+++ make_img.sh
@@ -1,24 +1,8 @@
 #!/bin/sh
 
-## make_img.sh ##
-
 # Creates a zipped & partitioned image file for burning onto SD cards
 # 256MB bootable FAT32L partition with official Alpine linux and lncm-box
 # Make sure "parted", "dosfstools" and "zip" are installed
-
-#   Copyright © 2018-2019 LNCM Contributors
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
 
 OUTPUT_VERSION=v0.4.1
 DOWNLOAD_VERSION=v0.4.0
