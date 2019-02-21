65a491dca57bbbd799a8943c130d212fd44d295a
AnotherDroog <36770425+AnotherDroog@users.noreply.github.com>
2019-02-22 00:42:25 +0700

    Output compressed container tarballs

--- make_container.sh
+++ make_container.sh
@@ -6,12 +6,25 @@ get_id() {
   docker ps -aqf "name=${1}"
 }
 
+export iotwifi_ID=`get_id iotwifi`
+export nginx_ID=`get_id nginx`
+
 save_container() {
   docker save -o ${1}.tar.gz ${2}
 }
 
-export iotwifi_ID=`get_id iotwifi`
-export nginx_ID=`get_id nginx`
+if [ ! -d lncm-workdir ]; then
+  mkdir lncm-workdir
+fi
 
-save_container iotwifi ${iotwifi_ID}
-save_container nginx ${nginx_ID}
+cd lncm-workdir
+
+save_container iotwifi cjimti/iotwifi
+save_container nginx nginx
+
+if [ ! -d output ]; then
+  mkdir output
+fi
+
+tar cvzf output/iotwifi.tar.gz iotwifi.tar.gz
+tar cvzf output/nginx.tar.gz nginx.tar.gz
