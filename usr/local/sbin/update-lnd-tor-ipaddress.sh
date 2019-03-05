fea314a7e07085009c0e0be94cec6f74e7cc4189
BT <nolim1t@users.noreply.github.com>
2019-03-05 15:31:47 +0700

    Add license

--- usr/local/sbin/update-lnd-tor-ipaddress.sh
+++ usr/local/sbin/update-lnd-tor-ipaddress.sh
@@ -2,6 +2,19 @@
 # if the node LAN IP changes (or box is plugged into a new LAN), lnd is unable to obviously connect to the new TOR IP address.
 # Basically this is an edge case right now as the merchant box should always have the same LAN IP.
 
+# Copyright © 2018-2019 LNCM Contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Look for existance of .ipaddress.txt
