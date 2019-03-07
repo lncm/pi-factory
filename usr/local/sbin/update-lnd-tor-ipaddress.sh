bf6e645f55c97fed1b5f973d6c7a30688f5d659c
AnotherDroog <36770425+AnotherDroog@users.noreply.github.com>
2019-03-07 20:34:31 +0700

    Fix paths

--- usr/local/sbin/update-lnd-tor-ipaddress.sh
+++ usr/local/sbin/update-lnd-tor-ipaddress.sh
@@ -23,14 +23,14 @@ IP=$(ip route get 1 | awk '{print $NF;exit}')
 if [ ! -f /home/lncm/.ipaddress.txt ]; then
   # If doesn't exist set it up
   echo "$IP" > /home/lncm/.ipaddress.txt
-  cp /media/important/lnd/lnd.conf /media/important/lnd.conf.old
+  cp /media/important/important/lnd/lnd.conf /media/important/important/lnd.conf.old
   # replace TORIPADDRESS in config with the IP
-  sed "s/TORIPADDRESS/$IP/g; " /media/important/lnd/lnd.conf
+  sed "s/TORIPADDRESS/$IP/g; " /media/important/important/lnd/lnd.conf
 else
   OLDIP=$(cat /home/lncm/.ipaddress.txt)
   if [ ! "$OLDIP" = "$IP" ]; then
-    cp /media/important/lnd/lnd.conf /media/important/lnd.conf.old
-    sed "s/$OLDIP/$IP/g; " /media/important/lnd/lnd.conf
+    cp /media/important/important/lnd/lnd.conf /media/important/important/lnd.conf.old
+    sed "s/$OLDIP/$IP/g; " /media/important/important/lnd/lnd.conf
     echo "$IP" > /home/lncm/.ipaddress.txt
   fi
 fi
