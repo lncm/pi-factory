66e3adfba91f379ce94bc2ddc58f54a195ead42c
nolim1t <hello@nolim1t.co>
2020-01-26 10:21:35 +0700

    Update to latest Alpine

--- make_img.sh
+++ make_img.sh
@@ -4,14 +4,15 @@
 # 256MB bootable FAT32L partition with official Alpine linux and lncm-box
 # Make sure "parted", "dosfstools" and "zip" are installed
 
-OUTPUT_VERSION=v0.5.2 # For Outputing an image
-REL=v3.10 # Which alpine release directory
+OUTPUT_VERSION=v0.5.3 # For Outputing an image
+REL=v3.11 # Which alpine release directory (Grab from https://www.alpinelinux.org/downloads/)
+
 
 # For fetching Alpine
 ARCH=aarch64
 ARCH32=armhf
-ALP=alpine-rpi-3.10.2-${ARCH}.tar.gz
-ALP32=alpine-rpi-3.10.2-${ARCH32}.tar.gz
+ALP=alpine-rpi-3.11.3-${ARCH}.tar.gz
+ALP32=alpine-rpi-3.11.3-${ARCH32}.tar.gz
 IMG=lncm-box-${OUTPUT_VERSION}-${ARCH}.img
 IMG32=lncm-box-${OUTPUT_VERSION}-${ARCH32}.img
 MNT=/mnt/lncm
